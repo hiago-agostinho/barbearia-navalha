1ea9a49d6836b2d700a3da3b24d77710a8d4b4c9
Hiago Agostinho <97325933+hiago-agostinho@users.noreply.github.com>
2023-10-19 14:10:56 +0000

    Adicionando rolagem suave ao projeto e criando component de horarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HorariosComponent } from './horarios/horarios.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HorariosComponent
   ],
   imports: [
     BrowserModule,
