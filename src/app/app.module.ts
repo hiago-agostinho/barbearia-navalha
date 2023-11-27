85e3a5284962c187b4064c31d733c51b14c501d1
Hiago Agostinho <97325933+hiago-agostinho@users.noreply.github.com>
2023-11-27 20:01:07 -0300

    Adicionando formulario para agendamento de horário

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HorariosComponent } from './horarios/horarios.component';
+import { FormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HorariosComponent } from './horarios/horarios.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/horarios/horarios.component.ts
+++ src/app/horarios/horarios.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './horarios.component.html',
   styleUrls: ['./horarios.component.scss']
 })
-export class HorariosComponent {
 
+export class HorariosComponent {
+  horarioData: string = '';
+  horarioHora: string = '';
+  horarioNome: string = '';
+  horarioTelefone: string = '';
+  horarioBarbeiro: string = '';
+
+  barbeiros = ['Adriano Rodrigues', 'Hiago Agostinho', 'Cleilton Rocha'];
+
+  submitForm() {
+    console.log('Agendamento:', this.horarioNome, this.horarioTelefone, this.horarioData, this.horarioHora, this.horarioBarbeiro);
+  }
 }
